94a735701f5b0c1e2d6142564c1b81b04360d7c5
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-01-24 15:14:20 -0300

    Esqueci de corrigir as sugestoes

--- js/sugestoes.js
+++ js/sugestoes.js
@@ -1,11 +1,21 @@
 let sugestoes = [
     'Júlio César',
+    'Origem de Roma',
     '10 fatos sobre Júlio César',
+    '10 fatos sobre a origem de Roma',
     'Top 10 fatos sobre Júlio César',
+    'Top 10 fatos sobre a origem de Roma',
+    'Como surgiu Roma',
+    'Como surgiram os romanos'
 ]
 
 const redirecionamentos = {
     'Júlio César': 'post.html?top-10-fatos-sobre-julio-cesar',
+    'Origem de Roma': 'post.html?origem-de-roma',
     '10 fatos sobre Júlio César': 'post.html?top-10-fatos-sobre-julio-cesar',
+    '10 fatos sobre a origem de Roma': 'post.html?origem-de-roma',
     'Top 10 fatos sobre Júlio César': 'post.html?top-10-fatos-sobre-julio-cesar',
+    'Top 10 fatos sobre a origem de Roma': 'post.html?origem-de-roma',
+    'Como surgiu Roma': 'post.html?origem-de-roma',
+    'Como surgiram os romanos': 'post.html?origem-de-roma'
 };
